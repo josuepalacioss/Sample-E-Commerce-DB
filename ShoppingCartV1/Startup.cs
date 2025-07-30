5c59a6e15d00ffb93be29887138b4704f26b7095
josuepalacioss <jpalacios_ec@hotmail.com>
2025-07-30 14:39:41 -0400

    Add files via upload

---
+++ ShoppingCartV1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingCartV1.Startup))]
+namespace ShoppingCartV1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
